21ca6740f94f55aba77841ecba233f3669f4a106
TommyCodet <tommycodet247@gmail.com>
2026-06-18 15:56:23 +0200

    Create BackpackGUI.java

---
+++ src/main/java/de/novateams/gui/BackpackGUI.java
@@ -0,0 +1,14 @@
+// src/main/java/de/novateams/gui/BackpackGUI.java
+package de.novateams.gui;
+
+// Backpack wird direkt über BackpackManager geöffnet
+// Diese Klasse ist nur ein Platzhalter für zukünftige Erweiterungen
+
+import de.novateams.NovaTeams;
+
+public class BackpackGUI {
+    
+    public BackpackGUI(NovaTeams plugin) {
+        // Backpack-Logik in BackpackManager
+    }
+}

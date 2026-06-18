e2e710aff6f47b5bf3b05dbd88202ae92c0ffd90
TommyCodet <tommycodet247@gmail.com>
2026-06-18 16:32:07 +0200

    Update EliteMobsHook.java

--- src/main/java/de/novateams/hooks/EliteMobsHook.java
+++ src/main/java/de/novateams/hooks/EliteMobsHook.java
@@ -1,39 +1,46 @@
-// src/main/java/de/novateams/hooks/EliteMobsHook.java
 package de.novateams.hooks;
 
+import com.magmaguy.elitemobs.api.EliteMobDeathEvent;
 import de.novateams.NovaTeams;
 import de.novateams.models.Team;
-import com.magmaguy.elitemobs.api.EliteMobDeathEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class EliteMobsHook implements Listener {
-    
+
     private final NovaTeams plugin;
-    
+
     public EliteMobsHook(NovaTeams plugin) {
         this.plugin = plugin;
     }
-    
+
     public void register() {
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
-    
+
     @EventHandler
     public void onEliteMobDeath(EliteMobDeathEvent event) {
+
         if (!(event.getEliteEntity().getDamagers().getTopDamager() instanceof Player player)) {
             return;
         }
-        
+
         Team team = plugin.getTeamCache().getByPlayer(player.getUniqueId());
-        if (team == null) return;
-        
+
+        if (team == null) {
+            return;
+        }
+
         int level = event.getEliteEntity().getLevel();
-        int xpPerLevel = plugin.getConfigManager().getConfig().getInt("xp.elitemobs.boss-kill-per-level", 5);
-        int totalXp = level xpPerLevel;
-        
+
+        int xpPerLevel = plugin.getConfigManager()
+                .getConfig()
+                .getInt("xp.elitemobs.boss-kill-per-level", 5);
+
+        int totalXp = level * xpPerLevel;
+
         plugin.getTeamManager().addXp(team, totalXp, "EliteMobs Boss");
     }
 }
